c72a5e261d846cc58974e0bd85573b9afcd2046b
ashpreetsingh72112 <69754940+ashpreetsinghanand@users.noreply.github.com>
2023-09-29 22:01:10 +0530

    small view 1

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
 import HeroImage from "@/images/heroImage.png"
+import HeroImageM from "@/images/HeroImageM.png"
 
 export default function Hero() {
   return (
     <div className='bg-homeHero1 min-h-[660px] pt-32'>
-        <div className='flex justify-center px-16'>
-            <div className='font-Progress basis-1/2 justify-center gap-5 flex flex-col'>
-                <h1 className='text-5xl'>OGCLUB: PIONEERING THE OG <span className='text-[#FA0300]'> DECENTRALIZED TOMORROW</span></h1>
-                <p className='font-OssemRust'>Join the OG Global Community of Innovators</p>
-                <button className='mt-2 hover:scale-110  duration-300 ease-in-out  text-xl p-1.5 px-3.5 drop-shadow-xl shadow-xl bg-[#FA0300] w-fit btn-pop'>EMBRACE OG INNOVATION</button>
+        <div className='flex flex-col md:flex-row justify-center px-6 md:px-16'>
+            <div className='font-Progress basis-1/2 justify-center gap-2 md:gap-5 flex flex-col'>
+                <h1 className='text-2xl text-center md:text-left md:text-5xl'>OGCLUB: PIONEERING THE OG <span className='text-[#FA0300]'> DECENTRALIZED TOMORROW</span></h1>
+                <p className='font-OssemRust text-center md:text-left my-3 md:my-0 text-[14px] md:text-[16px] '>Join the OG Global Community of Innovators</p>
+                <button className='mt-2 hover:scale-110 mb-10 md:mb-0 text-center duration-300 ease-in-out text-[14px] md:text-xl p-2   px-3  drop-shadow-xl shadow-xl bg-[#FA0300] md:w-fit btn-pop'>EMBRACE OG INNOVATION</button>
             </div>
-            <div>
-                <Image src={HeroImage} alt="EMBRACE OG INNOVATION" />
+             {/* Use CSS media query to conditionally show the image */}
+            <div className='hidden md:block'> {/* Hidden on small screens */}
+              <Image src={HeroImage} alt="EMBRACE OG INNOVATION" />
+            </div>
+            <div className='md:hidden flex items-center justify-center'> {/* Shown only on small screens */}
+              <Image src={HeroImageM} alt="EMBRACE OG INNOVATION" />
             </div>
         </div>
     </div>
--- src/components/Home/Roadmap.jsx
+++ src/components/Home/Roadmap.jsx
@@ -5,32 +5,81 @@ import RoadmapHexagon from './RoadmapHexagon'
 
 export default function Roadmap() {
   return (
-    <div className='px-12'>
-        <h1 className='font-Progress text-5xl text-center '>
+    <div className=' md:px-12'>
+        <h1 className='font-Progress text-[20px] md:text-5xl text-center '>
             OG ROADMAP
         </h1>
-        <div className='flex justify-evenly gap-4 pt-12'>
-            <div className='z-40'>
-                <RoadmapHexagon no="1" year="2020" />
+        <div className='md:flex  md:justify-evenly md:gap-4 md:pt-12'>
+            <div className='z-40 flex justify-center items-center'>
+                <RoadmapHexagon no="1" year="2020"  />
             </div>
-            <div className='w-[0.5px] h-52 mt-24 z-20  -rotate-45 bg-white ' />
-            <div className='z-40 mt-32'>
+
+            <div className='hidden md:block'> {/* Hidden on small screens */}
+                    <div className='w-[0.5px]   md:h-52 mt-10 md:mt-24 z-20 border -rotate-[45] md:-rotate-45 bg-white ' /> {/* <Image src={HeroImage} alt="EMBRACE OG INNOVATION" /> */}
+            </div>
+            <div className='md:hidden flex items-center justify-center'> {/* Shown only on small screens */}
+               <div className='w-[0.5px] h-40  z-20 border -rotate-[45] md:-rotate-45 bg-white ' />
+            </div>
+
+
+
+            <div className='z-40  mt-4 md:mt-32  flex justify-center items-center'>
                 <RoadmapHexagon no="2" year="2021" />
             </div>
-            <div className='w-[0.5px] h-52 mt-24 z-20  rotate-45 bg-white ' />
-            <div className='z-40 '>
+
+            <div className='hidden md:block'> {/* Hidden on small screens */}
+                <div className='w-[0.5px] md:h-52 md:mt-24 z-20  md:rotate-45 bg-white ' /> {/* <Image src={HeroImage} alt="EMBRACE OG INNOVATION" /> */}
+            </div>
+
+            <div className='md:hidden flex items-center justify-center'> {/* Shown only on small screens */}
+               <div className='w-[0.5px] h-40  z-20 border -rotate-[45] md:-rotate-45 bg-white ' />
+            </div>
+            
+            <div className='z-40 flex justify-center items-center'>
                 <RoadmapHexagon no="3" year="2022" />
             </div>
-            <div className='w-[0.5px] h-52 mt-24 z-20  -rotate-45 bg-white ' />
-            <div className='z-40 mt-32'>
+
+
+             <div className='hidden md:block'> {/* Hidden on small screens */}
+                    <div className='w-[0.5px]   md:h-52 mt-10 md:mt-24 z-20 border -rotate-[45] md:-rotate-45 bg-white ' /> {/* <Image src={HeroImage} alt="EMBRACE OG INNOVATION" /> */}
+            </div>
+            <div className='md:hidden flex items-center justify-center'> {/* Shown only on small screens */}
+               <div className='w-[0.5px] h-40  z-20 border -rotate-[45] md:-rotate-45 bg-white ' />
+            </div>
+
+
+
+            <div className='z-40  mt-4 md:mt-32  flex justify-center items-center'>
                 <RoadmapHexagon no="4" year="2022" />
             </div>
-            <div className='w-[0.5px] h-52 mt-24 z-20  rotate-45 bg-white ' />
-            <div className='z-40 '>
+            
+            <div className='hidden md:block'> {/* Hidden on small screens */}
+                <div className='w-[0.5px] md:h-52 md:mt-24 z-20  md:rotate-45 bg-white ' /> {/* <Image src={HeroImage} alt="EMBRACE OG INNOVATION" /> */}
+            </div>
+
+            <div className='md:hidden flex items-center justify-center'> {/* Shown only on small screens */}
+               <div className='w-[0.5px] h-40  z-20 border -rotate-[45] md:-rotate-45 bg-white ' />
+            </div>
+
+            <div className='z-40 mt-4 flex justify-center items-center'>
                 <RoadmapHexagon no="5" year="2023" />
             </div>
-            <div className='w-[0.5px] h-52 mt-24 z-20  -rotate-45 bg-white ' />
-            <div className='z-40 mt-32'>
+
+
+
+
+            
+            <div className='hidden md:block'> {/* Hidden on small screens */}
+                    <div className='w-[0.5px]   md:h-52 mt-10 md:mt-24 z-20 border -rotate-[45] md:-rotate-45 bg-white ' /> {/* <Image src={HeroImage} alt="EMBRACE OG INNOVATION" /> */}
+            </div>
+
+            <div className='md:hidden flex items-center justify-center'> {/* Shown only on small screens */}
+               <div className='w-[0.5px] h-40  z-20 border -rotate-[45] md:-rotate-45 bg-white ' />
+            </div>
+
+
+
+            <div className='z-40  mt-4 md:mt-32  flex justify-center items-center'>
                 <RoadmapHexagon no="6" year="2024" />
             </div>
         </div>
