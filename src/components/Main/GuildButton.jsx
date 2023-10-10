0cf9f78c7f58a9ccbccf44660a6e99f52b55c033
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2023-10-10 23:00:24 +0530

    dev: Landing page button resizing

--- src/components/Main/GuildButton.jsx
+++ src/components/Main/GuildButton.jsx
@@ -10,7 +10,7 @@ import {BsTriangleFill} from "react-icons/bs"
 export default function GuildButton(props) {
     const photo = props.no === "1" ? FounderGuild : props.no === "2" ? DevGuild : props.no === "3" ? WomenGuild : props.no === "4" ? GamerGuild : FounderGuild
   return (
-    <div className='flex w-52  md:w-auto relative group items-center hover:scale-105 duration-300 ease-in-out btn-pop gap-2 drop-shadow-xl shadow-xl p-2 border border-[#707070] bg-black/50 rounded-lg w-fit'>
+    <div className='flex w-52 md:w-auto relative group items-center hover:scale-105 duration-300 ease-in-out btn-pop gap-2 drop-shadow-xl shadow-xl p-2 border border-[#707070] bg-black/50 rounded-lg md:w-fit '>
         <div>
             <Image src={photo} alt={props.title} />
         </div>
